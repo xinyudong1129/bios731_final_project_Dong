dc18278a4906ebc32164c66a3498427a3f1400c1
Dong <XDONG63@emory.edu>
2026-04-20 12:06:21 -0400

    added cgm simulation code

---
+++ R/simulate_cgm.R
@@ -0,0 +1,49 @@
+simulate_cgm_panel <- function(n_patients = 50,
+                               n_time = 288,
+                               interval_mins = 5,
+                               seed = 123) {
+  set.seed(seed)
+  
+  patient_ids <- sprintf("P%03d", seq_len(n_patients))
+  start_time <- as.POSIXct("2024-01-01 00:00:00", tz = "UTC")
+  
+  sim_list <- lapply(patient_ids, function(id) {
+    t_idx <- seq_len(n_time)
+    tt <- start_time + (t_idx - 1) * interval_mins * 60
+    
+    patient_intercept <- rnorm(1, 110, 15)
+    circadian <- 15 * sin(2 * pi * t_idx / (24 * 60 / interval_mins))
+    ar_noise <- as.numeric(stats::arima.sim(model = list(ar = 0.7), n = n_time, sd = 8))
+    glucose_true <- patient_intercept + circadian + ar_noise
+    
+    lag_glucose <- dplyr::lag(glucose_true, default = glucose_true[1])
+    p_missing_start <- plogis(-5 + 0.015 * lag_glucose + 0.6 * (lubridate::hour(tt) %in% c(0,1,2,3)))
+    missing_start <- rbinom(n_time, 1, p_missing_start)
+    
+    duration_steps <- ifelse(
+      missing_start == 1,
+      pmax(1, round(exp(rnorm(n_time, log(6), 0.4)))),
+      0
+    )
+    
+    glucose_obs <- glucose_true
+    i <- 1
+    while (i <= n_time) {
+      if (missing_start[i] == 1) {
+        end_i <- min(n_time, i + duration_steps[i] - 1)
+        glucose_obs[i:end_i] <- NA_real_
+        i <- end_i + 1
+      } else {
+        i <- i + 1
+      }
+    }
+    
+    tibble::tibble(
+      patient_id = id,
+      time = tt,
+      glucose = glucose_obs
+    )
+  })
+  
+  dplyr::bind_rows(sim_list)
+}
